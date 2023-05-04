9a0355d01f5f3e20917a23d4cf964410c2f4098b
김용민 <dydals3440@gmail.com>
2023-05-05 00:22:29 +0900

    style: Header.module.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import './Header.module.css';
+import styles from './Header.module.css';
 
 export default function Header({ filters, filter, onFilterChange }) {
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
-            <button onClick={() => onFilterChange(value)}>{value}</button>
+            <button
+              className={`${styles.filter} ${
+                filter === value && styles.selected
+              }`}
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
           </li>
         ))}
       </ul>
