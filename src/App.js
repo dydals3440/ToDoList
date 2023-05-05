72d205590f9cd232befeb96718f7b7b511031ae4
김용민 <dydals3440@gmail.com>
2023-05-05 15:03:17 +0900

    feat: 다크모드 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TodoList from './components/ToDoList/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const filters = ['All', 'Active', 'Completed'];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header
         filters={filters}
         filter={filter}
         onFilterChange={(filter) => setFilter(filter)}
       />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
